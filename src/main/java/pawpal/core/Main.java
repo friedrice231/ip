30c42ec1ce257ab1e5e3e7635d23c9183df2fef9
friedrice231 <root@Magnus.>
2025-02-13 23:03:12 +0800

    Add an icon and title to the app

--- src/main/java/pawpal/core/Main.java
+++ src/main/java/pawpal/core/Main.java
@@ -1,10 +1,12 @@
 package pawpal.core;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,6 +21,8 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            Image icon =
+                    new Image(String.valueOf(Objects.requireNonNull(Main.class.getResource("/images/cat-icon.png"))));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
@@ -26,6 +30,8 @@ public class Main extends Application {
             stage.setMinHeight(220);
             stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setPawPal(pawpal); // inject the Duke instance
+            stage.getIcons().add(icon);
+            stage.setTitle("PawPal");
             stage.show();
         } catch (IOException e) {
             System.out.println(e.getMessage());;
